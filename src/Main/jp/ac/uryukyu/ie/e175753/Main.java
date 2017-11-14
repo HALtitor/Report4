d52f7b4b5986e514acb2baf1cee822d09e640f97
Yamakawa Haruki <e175753@ie.u-ryukyu.ac.jp>
2017-11-14 09:36:20 +0900

    second commit

--- src/Main/jp/ac/uryukyu/ie/e175753/Main.java
+++ src/Main/jp/ac/uryukyu/ie/e175753/Main.java
@@ -2,8 +2,14 @@ package jp.ac.uryukyu.ie.e175753;
 
 public class Main {
     public static void main(String args[]){
+        try{
         String str = null;
-        System.out.println(str.length());
+            System.out.println(str.length());
+        }catch (NullPointerException e){
+            System.out.println("NullPointerExceptionが発生");
+            System.out.println(e.getMessage());
+        }
+
     }
 
 }
